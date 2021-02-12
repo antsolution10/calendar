2b3ae11c1a68f7dce397b56f4f6a39bc8ad6bce2
antsolution10 <antsolution10@gmail.com>
2021-02-12 18:25:06 +0900

    s

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,74 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_clean_calendar/flutter_clean_calendar.dart';
-void main() => runApp(MyApp());
+
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(
-            title: Center(child: Text("AntSolution")),
-          ),
-          body: Center(child: CalendarScreen()),
-        ));
-  }
-
-}
-
-
-class CalendarScreen extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    return _CalendarScreenState();
-  }
-}
-
-class _CalendarScreenState extends State<CalendarScreen> {
-  void _handleNewDate(date) {
-    setState(() {
-      _selectedDay = date;
-      _selectedEvents = _events[_selectedDay] ?? [];
-    });
-    print(_selectedEvents);
-  }
-
-  List _selectedEvents;
-  DateTime _selectedDay;
-
-  final Map<DateTime, List> _events = {
-    DateTime(2021, 1, 25): [
-      {'Memo': 'Data A', 'isDone': true},
-      {'Memo': 'Data B', 'isDone': true},
-      {'Memo': 'Data C', 'isDone': false},
-      {'Memo': 'Data D', 'isDone': false},
-      {'Memo': 'Data E', 'isDone': false},
-    ],
-
-  };
-
-  @override
-  void initState() {
-    super.initState();
-    _selectedEvents = _events[_selectedDay] ?? [];
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Column(
-          mainAxisSize: MainAxisSize.max,
-          children: <Widget>[
-            Container(
-              child: Calendar(
-                startOnMonday: true,
-                weekDays: ["월", "화", "수", "목", "금", "토", "일"],
-                events: _events,
-                onRangeSelected: (range) =>
-                    print("Range is ${range.from}, ${range.to}"),
-                onDateSelected: (date) => _handleNewDate(date),
-                isExpandable: true,
-                eventDoneColor: Colors.green,
-                selectedColor: Colors.pink,
-                todayColor: Colors.blue,
-                eventColor: Colors.grey,
-                dayOfWeekStyle: TextStyle(
-                    color: Colors.black,
-                    fontWeight: FontWeight.w800,
-                    fontSize: 11),
-              ),
-            ),
-            _buildEventList()
+      home: Scaffold(
+        body: Column(
+          children: [
+            textSection,
+            SizedBox(height: 70,
+                child: CalendarDay),
+            Expanded(child: Calendar)
           ],
         ),
       ),
     );
   }
-
-  Widget _buildEventList() {
-    return Expanded(
-      child: ListView.builder(
-        itemBuilder: (BuildContext context, int index) => Container(
-          decoration: BoxDecoration(
-            border: Border(
-              bottom: BorderSide(width: 3, color: Colors.black12),
-            ),
-          ),
-          padding: const EdgeInsets.symmetric(horizontal: 4.0, vertical: 4.0),
-          child: ListTile(
-            title: Text(_selectedEvents[index]['Memo'].toString()),
-            onTap: () {},
-          ),
-        ),
-        itemCount: _selectedEvents.length,
-      ),
-    );
-  }
 }
 
 
+  final String name = "";
+
+  Widget Calendar  = GridView.count(
+  crossAxisCount: 7,
+  children: List.generate(42, (index) {
+    String day;
+    String startDay="금";
+
+    if(startDay == "월")
+      index = index;
+    else if(startDay == "금")
+        index = index - 4;
+
+    day = index.toString();
+
+    if(index <= 0 || index > 31)
+      day = "";
+
+    return Center(
+      child: Text(
+        day
+         ,
+      ),
+    );
+  })
+);
+
+Widget CalendarDay  =  GridView.count(
+
+    crossAxisCount: 7,
+    children: List.generate(7, (index) {
+      var Day =["일","월","화","수","목","금","토"];
+      return Center(
+        child: Text(
+         Day[index],
+        ),
+      );
+    })
+);
+
+
+Widget textSection = Container(
+  padding: const EdgeInsets.all(32),
+  child: Text(
+    '2021년 1월',
+    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+  ),
+);
